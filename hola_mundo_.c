3f101b7906ee95f81217aebe1bbe1d191eca3996
lujeolhe <lujeolhe@live.com.mx>
2020-11-24 14:39:26 -0600

    Se agregan los comentarios de la estructura basica del prograna

--- hola_mundo_.c
+++ hola_mundo_.c
@@ -1,5 +1,46 @@
+/*Header (Encabezado)
+*Autor: Ulises,Luis
+*Fecha: 22/11/2020
+*Descripcion: Mi primer programa en C
+*/
+/////////////////////////////////////
+//					Bibliotecas
+/////////////////////////////////////
 #include<stdio.h>
-void main (){
-	printf("hola mundo\n");
-	printf("Hola que tal Ulises\n");
+/////////////////////////////////////
+//					Definiciones
+/////////////////////////////////////
+#define constante 65
+#define constanteChar 'a'
+
+/////////////////////////////////////
+//			Esqueleto de Funciones
+/////////////////////////////////////
+void tercearia();
+void secundaria();
+/////////////////////////////////////
+//					Funcion Principal
+/////////////////////////////////////
+int main(){
+	printf("hola mundo: %c \n",constante);
+	printf("Hola que tal Ulises %c\n",constanteChar);
+
+	tercearia();
+	printf("Se terminaron de ejecutar las funciones\n");
+	return 1;
+}
+
+/////////////////////////////////////
+//					Definicion de Funciones
+/////////////////////////////////////
+void tercearia(){
+	// Definicion
+	secundaria();
+	printf("hola mundo, soy la Funcion tercearia: \n");
+	//
+}
+void secundaria(){
+	// Definicion
+	printf("hola mundo, soy la Funcion Secundaria: \n");
+	//
 }
